5793a37bb5a453566887b54b981cef45b37256a4
George Ramirez <george.ramirez2@gmail.com>
2018-01-02 11:02:57 -0700

    trying to get service worker to register as production, not working

--- sw-register.js
+++ sw-register.js
@@ -1,10 +1,9 @@
 if ('serviceWorker' in navigator){
-  navigator.serviceWorker.register(
-    'sw.js',
-    {
-    scope:'/'
-  }
-  ).then((registration) => {
-    console.log('Service worker registation completed')
-  })
-}
+  // console.log('working')
+  navigator.serviceWorker.register('sw.js',
+    {scope:'/'})
+      .then((registration) => {
+        console.log('Service worker registation completed')
+      }
+  )
+}else{console.log('no serviceWorker in navigator')}
